ac97b883103eac62fd362330bedf5cbfe183abce
ianiemcz <71410571+ianiemcz@users.noreply.github.com>
2024-11-15 16:14:46 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration with ENV variables. Set default values as well 
-$db_host = getenv('DB_HOST') ?: _______;
+$db_host = getenv('DB_HOST') ?: '';
 $db_name = getenv('DB_NAME') ?: 'image_catalog';
 $db_user = getenv('DB_USER') ?: 'appmod-phpapp-user';
-$db_pass = getenv('DB_PASS') ?: _______;
+$db_pass = getenv('DB_PASS') ?: '';
 // Note getenv() is PHP 5.3 compatible
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
