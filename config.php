53ac27773a67c5ed5331be1ab7c89217c0162091
WageSlave007 <123157446+WageSlave007@users.noreply.github.com>
2024-11-15 14:33:52 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
-$db_host = 'localhost';
+$db_host = '34.88.52.110';
 $db_name = 'image_catalog';
-$db_user = 'root';
-$db_pass = 'veryverystrongpassword';
+$db_user = 'appmod-phpapp-user';
+$db_pass = 'kFdZB-z+NizpXF1';
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
